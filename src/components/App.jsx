45ae71e253528ee4d024fc46ae1000c94e056cb8
Juan David Álvarez Sarmiento <75177549+JDavid550@users.noreply.github.com>
2021-04-16 00:27:29 -0500

    Trae la api de forma dinamica

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,23 +10,43 @@ class App extends React.Component{
         this.state ={
             loading:false,
             error:null,
-            data:{}
+            data:{},
+            parameter: 'Bogota',
+            date :{
+                date:new Date().toDateString(),
+                time: new Date().toLocaleTimeString()
+            }  
+            
         }
-        this.API = 'https://api.openweathermap.org/data/2.5/weather?q=bogota&units=metric&appid=04796f77ba3f29c9fdadc0f4ed34314a'
+
+        //this.API = `https://api.openweathermap.org/data/2.5/weather?q=bogota&units=metric&appid=04796f77ba3f29c9fdadc0f4ed34314a`
 
         this.fetchData = async()=>{
 
             this.setState({loading:true})
             try {
-                const response = await fetch(this.API)
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.parameter}&units=metric&appid=04796f77ba3f29c9fdadc0f4ed34314a`)
                 const data = await response.json()
                 console.log(data)
+                const secondResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,current,hourly,alerts,minutely&units=metric&appid=04796f77ba3f29c9fdadc0f4ed34314a`)
+                const secondData = await secondResponse.json()
+                console.log(secondData)
                 this.setState({loading:false, data})
             } catch (error) {
                 console.log('fetch error', error)
             }
             
         }
+
+        this.handleChange = (e)=>{
+            this.setState({parameter: e.target.value})
+        }
+
+        this.handleClick = () => {
+            this.fetchData()
+        }
+        
+        
     }
     
     componentDidMount(){
@@ -42,18 +62,24 @@ class App extends React.Component{
         return <div className="Container">
 
             <Container_result 
-                Temperature = {this.state.data.main.temp}
-                Description = {this.state.data.weather[0].description} />
+                Temperature = {this.state.data.main.temp || 'Temperature'} 
+                Description = {this.state.data.weather[0].description || 'Description'}
+                City = {this.state.parameter}
+                onChange = {this.handleChange}
+                onClick = {this.handleClick}
+                Date = {this.state.date.date}
+                Time = {this.state.date.time} />
 
             <div className="Container__dashboard">
                 <h2 className="highlights">The next days forecast</h2>
                 <hr className="line"/>
                 <Dashboard_forecast/>
+
                 <Dashboard_result 
-                    Pressure= {this.state.data.main.pressure}
-                    Humidity= {this.state.data.main.humidity}
-                    Clouds= {this.state.data.clouds.all}
-                    WindSpeed= {this.state.data.wind.speed}/>
+                    Pressure= {this.state.data.main.pressure || 'Pressure'}
+                    Humidity= {this.state.data.main.humidity || 'Humidity'}
+                    Clouds= {this.state.data.clouds.all || 'Clouds'}
+                    WindSpeed= {this.state.data.wind.speed || 'WindSpeed'}/>
                 
             </div>
         </div>
--- src/components/Container_result.jsx
+++ src/components/Container_result.jsx
@@ -6,14 +6,25 @@ class Container_result extends React.Component{
     render(){
         return <div className="Container__result">
         <div className="Search_bar">
-            <h2>Search</h2>
-            <h2>Search Button</h2>
+            <input 
+                type="text" 
+                id="parameter" 
+                placeholder="City"
+                onChange = {this.props.onChange}
+                />
+            <button onClick={this.props.onClick}>Search Button</button>
+            
         </div>
         <div className="Results__pane">
             <img src={logo} alt="Weather Image"/>
             <h1>{this.props.Temperature}</h1>
             <h2>{this.props.Description}</h2>
-            <h3>Date</h3>
+            <div className="Date">
+            <h3>{this.props.Date}</h3>
+            <h3>{this.props.Time}</h3> 
+            </div>
+            
+
             <h3>Credits</h3>
         </div>
     </div>
